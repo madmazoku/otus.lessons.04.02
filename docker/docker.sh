45c192cfe1e5760c5c4f0652479fe147111500fa
madmazoku <madmazoku@gmail.com>
2017-12-15 21:12:44 +0300

    update docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,7 @@
 echo ========= Build docker image
 docker build -t otus.lessons.04.02 .
+echo ========= Check ip_filter version
+docker run --rm -i otus.lessons.04.02 ip_filter -v
 echo ========= Filter IPs, result will go to ../bin/out.txt
 cat ../src/ip_filter.tsv | docker run --rm -i otus.lessons.04.02 ip_filter > ../bin/out.txt
 echo ========= Remove docker image
